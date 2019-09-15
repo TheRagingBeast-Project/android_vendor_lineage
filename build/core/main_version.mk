0c5bd2fdc654f0a0713c81b6f70b08c3d78ae005
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:52:51 +0530

    build: Ship as TRB based on LOS
    Signed-off-by: Varun.Vaishnav <gamelovr695@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,17 +4,13 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# theragingbeast System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
+    ro.theragingbeast.build.version=$(LINEAGE_VERSION) \
+    ro.theragingbeast.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.theragingbeast.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(TRB_VERSION) \
     ro.lineagelegal.url=https://lineageos.org/legal
-
-# LineageOS Platform Display Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
 
 # LineageOS Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
